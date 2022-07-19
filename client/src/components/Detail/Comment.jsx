185223bf1b3641a08cdb467a51ecb0c005b3e9fb
Xavicklo <Andy286460@gmail.com>
2022-07-19 15:50:56 +0800

    refactor

---
+++ client/src/components/Detail/Comment.jsx
@@ -0,0 +1,178 @@
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Box,
+} from "@mui/material";
+import Rating from "@mui/material/Rating";
+import { styled } from "@mui/material/styles";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { AdminContext } from "../Helper/Context";
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary: "white",
+    },
+    background: {
+      paper: "#424242",
+    },
+  },
+});
+const StyledRating = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#ff6d75",
+  },
+  "& .MuiRating-iconHover": {
+    color: "#ff3d47",
+  },
+  "& .MuiRating-iconEmpty": {
+    color: "gray",
+  },
+});
+const Comment = ({ info, handleDelete }) => {
+  const id = { uid: info.User_user_id1 };
+  const [username, setUsername] = useState("");
+  const [rating, setRating] = useState(0);
+  const [admin, setAdmin] = useContext(AdminContext);
+  const [likes, setLikes] = useState(0);
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/comment/postUsername", id)
+      .then((response) => {
+        setUsername(response.data.username);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // check if user like the post
+    axios
+      .post(
+        "http://localhost:3001/comment/checkLike",
+        { pid: info.post_id },
+        {
+          headers: { accessToken: sessionStorage.getItem("accessToken") },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.result) {
+          setRating(0);
+        } else {
+          setRating(1);
+        }
+      });
+    // total likes
+    axios
+      .post("http://localhost:3001/comment/totalLikes", { pid: info.post_id })
+      .then((response) => {
+        console.log(response.data);
+        setLikes(response.data[0].likes);
+      });
+  }, []);
+
+  // like post
+  const handleLike = (event, newValue) => {
+    if (newValue === 1) {
+      axios
+        .post(
+          "http://localhost:3001/comment/likePost",
+          { pid: info.post_id },
+          {
+            headers: { accessToken: sessionStorage.getItem("accessToken") },
+          }
+        )
+        .then((response) => {
+          if (response.data.result) {
+            console.log(response.data.result);
+            alert("Error: user didn't login");
+          } else {
+            console.log(response.data);
+            setRating(newValue);
+          }
+        });
+    } else {
+      //remove Like
+      axios
+        .post(
+          "http://localhost:3001/comment/removeLike",
+          { pid: info.post_id },
+          {
+            headers: { accessToken: sessionStorage.getItem("accessToken") },
+          }
+        )
+        .then((response) => {
+          if (response.data.result) {
+            console.log(response.data.result);
+            alert("Error: user didn't login");
+          } else {
+            console.log(response.data);
+            setRating(newValue);
+          }
+        });
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/comment/totalLikes", { pid: info.post_id })
+      .then((response) => {
+        console.log(response.data);
+        setLikes(response.data[0].likes);
+      });
+  }, [rating]);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Card sx={{ m: 2 }} style={{ borderRadius: 10, width: "100%" }}>
+          <CardHeader
+            title={username}
+            action={
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <StyledRating
+                  max={1}
+                  icon={<FavoriteIcon fontSize="inherit" />}
+                  emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+                  value={rating}
+                  onChange={handleLike}
+                />
+                <Typography variant="h5" sx={{ ml: 1 }}>
+                  {likes}
+                </Typography>
+                {admin === 1 && (
+                  <IconButton
+                    aria-label="close"
+                    sx={{ color: "#f9d3b4" }}
+                    onClick={() => handleDelete(info.post_id)}
+                  >
+                    <DeleteIcon sx={{ fontSize: "inherit" }} />
+                  </IconButton>
+                )}
+              </Box>
+            }
+          />
+          <CardContent>
+            <Typography variant="h5">{info.content}</Typography>
+          </CardContent>
+        </Card>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Comment;
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import Movie from "../Movies/Movie";
+import "../..//App.css";
+import axios from "axios";
+import { Box, Button, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const [movieName, setMovieName] = useState("");
+  const [avgs, setAvgs] = useState([]);
+  // default value will be leaderboard
+  const [movies, setMovies] = useState([]);
+  let navigate = useNavigate();
+  const genre = ["Action", "Romance", "Sci-Fi", "Comedy", "Drama"];
+
+  // get recommandation
+  useEffect(async () => {
+    axios
+      .get("http://localhost:3001/home/recommand")
+      .then((response) => {
+        setMovies(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const searchMovie = async (movieName) => {
+    await axios
+      .post("http://localhost:3001/home/search", { movieName: movieName })
+      .then((response) => {
+        setMovies(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    if (movieName.length !== 0) {
+      searchMovie(movieName);
+    }
+  }, [movieName]);
+
+  // get movie's score
+  useEffect(async () => {
+    const promises = movies.map(async (movie) => {
+      const score = await axios.post("http://localhost:3001/home/avg", {
+        mid: movie.movie_id,
+      });
+      return score.data._avg.score;
+    });
+    const scores = await Promise.all(promises);
+    setAvgs(scores);
+  }, [movies]);
+
+  // logout function
+  const logout = () => {
+    sessionStorage.clear();
+    navigate("/");
+  };
+
+  const findMovieByGenre = async (e) => {
+    await axios
+      .post("http://localhost:3001/home/genre", {
+        genre: e.currentTarget.value,
+      })
+      .then((response) => {
+        setMovies(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <div className="header">
+        <Grid container style={{ width: "100%" }}>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <h1>Movie Search</h1>
+            </Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "flex-start",
+                p: 1,
+                m: 1,
+                borderRadius: 5,
+              }}
+            >
+              <Button
+                variant="outlined"
+                color="secondary"
+                sx={{
+                  width: 125,
+                  color: "#f9d3b4",
+                  borderColor: "#f9d3b4",
+                  "&:hover": {
+                    backgroundColor: "#f9d3b4",
+                    color: "#212426",
+                    borderColor: "#f9d3b4",
+                  },
+                }}
+                onClick={logout}
+              >
+                logout
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search a movie"
+          value={movieName}
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
+      </div>
+      <Box
+        style={{ width: "100%" }}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {genre.map((g) => (
+          <Button
+            key={g}
+            value={g}
+            variant="outlined"
+            color="secondary"
+            sx={{
+              m: 2,
+              width: 125,
+              color: "#f9d3b4",
+              borderColor: "#f9d3b4",
+              "&:hover": {
+                backgroundColor: "#f9d3b4",
+                color: "#212426",
+                borderColor: "#f9d3b4",
+              },
+            }}
+            onClick={findMovieByGenre}
+          >
+            {g}
+          </Button>
+        ))}
+      </Box>
+      {movieName.length >= 0 && movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <Movie movie={movie} key={movie.movie_id} avg={avgs[index]} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>no movie found</h2>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
