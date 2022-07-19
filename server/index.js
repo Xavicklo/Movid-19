e6b65dd6f3235e115d5dc60633001cab6dd3e848
Xavicklo <Andy286460@gmail.com>
2022-07-19 15:51:24 +0800

    refactor

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { userRouter } from "./routes/user";
+import { homeRouter } from "./routes/home";
+import { detailRouter } from "./routes/detail";
+import { commentRouter } from "./routes/comment";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// choose port
+app.listen(3001, () => {
+  console.log("server run on port 3001");
+});
+
+// ******************* Login and Registration *********************************
+app.use("/user", userRouter);
+
+// ************************ Home ***********************************************
+app.use("/home", homeRouter);
+
+//  ************************** Detail/Rate **********************************
+app.use("/detail", detailRouter);
+
+//  ************************** Comment/Like **********************************
+app.use("/comment", commentRouter);
